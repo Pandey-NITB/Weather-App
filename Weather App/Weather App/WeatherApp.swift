5ef947ae6509ec2c01f5b7e28b3e19de49cd35c3
Prashant Pandey <prashant5.manit@gmail.com>
2026-03-31 20:54:50 +0530

    initial commit

---
+++ Weather App/Weather App/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_AppApp.swift
+//  Weather App
+//
+//  Created by Prashant Pandey on 31/03/26.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
